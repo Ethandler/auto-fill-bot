bdfb6835ca624e24e8d5812207f31c8a04f48420
EthanOS <ethan.b.freelance@gmail.com>
2025-04-03 19:44:33 -0700

    Longer list

--- main.py
+++ main.py
@@ -3,36 +3,35 @@ import pyperclip
 import pandas as pd
 import time
 
-#===This is my settings EB===
-delay_between_fields = 0.35
-start_delay = 4.5
+
+delay_between_fields = 0.65
+start_delay = 6.0
 csv_file = 'form_data.csv'
 
-#===This loads the data EB===
+
 data = pd.read_csv(csv_file)
 row = data.iloc[0] 
 
-fields = [row['Title'], 
-          row['Company Name'], 
-          row['Company State Location'], 
-          row['First Name'], 
-          row['Last name'], 
-          row['Phone Number'], 
-          row['Street Address'],  
-          row['Street Address Line 2'], 
-          row['City'], 
-          row['State / Province'], 
-          row['Postal / Zip Code']] 
+field_names = ['First Name', 'Last Name', 'Email', 'Street Address', 'Street Address Line 2',
+               'City', 'State / Province','Postal / Zip Code','Area Code','Phone number','Type a Question',]
+fields = []
 
+for name in field_names:
+    value = row.get(name, "")  
+    fields.append(value)
+
+print("Fields to be entered:", fields)
 
 print(f"Move your mouse to the first field for Fill to fill the form! Starting in {start_delay} seconds...")
 time.sleep(start_delay)
 
 #===INPUT LOOP EB===
 for field in fields:
-    pyperclip.copy(str(field)) #copy the field to the clipboard EB
-    pyautogui.hotkey('ctrl', 'v') #paste the field into the field EB
-    pyautogui.press('tab') #move to the next field EB
-    time.sleep(delay_between_fields) #wait a bit before moving between fields EB
+    pyperclip.copy(str(field))
+    time.sleep(delay_between_fields) 
+    pyautogui.hotkey('ctrl', 'v')
+    time.sleep(delay_between_fields)  
+    pyautogui.press('tab') 
+    time.sleep(delay_between_fields) 
     
     print("Fill filled the field!")
